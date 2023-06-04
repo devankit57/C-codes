835d39f9afcfce1e43f7ee5af957a63acc40b7b4
Ankit <83949374+devankit57@users.noreply.github.com>
2023-06-04 15:46:49 +0530

    Create 0.hello-world.cpp

---
+++ 0.hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>     
+// Main function: entry point of the program               
+int main() {   // Statements: code that performs specific actions   
+                 
+		 cout << "Hello, World!" <<endl;                    
+
+	  // Return statement: indicates the end of the program and returns an exit status                                     return 0;  
+
+}
